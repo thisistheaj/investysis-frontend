3d622b47c8acbfdc6f5a1b3618a1db4c8b55d40e
thisistheaj <ajbeckner@gmail.com>
2018-04-14 15:59:36 -0400

    stock page more UI

--- src/pages/stock/stock.ts
+++ src/pages/stock/stock.ts
@@ -17,7 +17,11 @@ import {Chart} from "chart.js";
 export class StockPage {
 
   @ViewChild('stockcanvas') lineCanvas;
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+
   public lineChart: any;
+  public doughnutChart: any;
+
   public ticker;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
@@ -56,8 +60,38 @@ export class StockPage {
           }
         ]
       }
+    });
+
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+
+      type: 'doughnut',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+      }
 
     });
+
   }
 
 }
